39b66435ccb7c93ece7e69a8074aa366955fa19e
marco <marco@MARCO>
2024-06-07 06:34:12 -0300

    Adicionar arquivos de projeto.

---
+++ Cartório21.Database/Conexão/ConexaoBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+
+namespace Cartório21.Database.Conexão
+{
+    public class ConexaoBase
+    {
+        readonly private string _connectionString = "Data Source=Marco\\SQLEXPRESS;Initial Catalog=Cartorio21_Database;Integrated Security=True;Encrypt=False";
+        public SqlConnection abrirConexaoBase()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
